95baa55367b312ff939c3b0a84b2b255f539c7c4
Jared Tulayan <jared@jaredtulayan.xyz>
2020-10-22 01:26:22 -0400

    Finish board generation

--- board.py
+++ board.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+# Board representation
+# Char board, with Originator structure
 class Board():
     _board = np.array([])
    
     # Initialize the board with some multiple d
     # The board will be 3dx3d large.
     def __init__(self, d):
-        size = 3d
+        size = 3 * d
         
         # Step 1: Initialize an empty board
         board = np.array([["_" for _ in range(size)] for _ in range(size)])
@@ -19,13 +21,18 @@ class Board():
         board[size - 1][1::3] = ["h" for _ in range(d)] 
         board[size - 1][2::3] = ["m" for _ in range(d)]
 
+        # Step 3: Populate with pits
+        board[range(1, size - 1), np.random.randint(size, size = size - 2)] = ["O" for _ in range(size - 2)]
+
+        # Step 4: Assign class member
         self._board = board
 
     def __repr__(self):
-        pass
+        return repr(self._board)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     board = Board(3)
+    print(board)
 
 
 
