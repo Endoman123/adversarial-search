7e18781e6e07d8e203f67d722c8df77f93c09f06
Ian Quinn <ianquinn15@gmail.com>
2020-12-18 21:48:19 -0500

    Added eval function

--- probability.py
+++ probability.py
@@ -37,8 +37,9 @@ def get_obs(board, prob_table, major):
 
 
 # Probabilistics AI
-def eval(**kwargs):
-   pass 
+def eval(prob_table, move):
+    (y, x) = move[1]
+    return sum(prob_table[u][y][x] for u in "WHM")
 
 
 # Updates all of the probability boards after the opponent has moved
@@ -112,80 +113,4 @@ def guess_move(board, major, prob_table, remaining, h = h_disable, w = 1):
             ret = move
             best = temp
 
-    return ret 
-
-
-# Function to update an probability table
-# c = # of remaining pieces for that specific unit
-# prob = the probability table
-# neighbors = a 2d table which counts the number of viable neighbors each cell on the board has
-def update_after_opp(c, prob, neighbors):
-    d = len(prob)
-    new_prob = prob
-
-    # Assumes that the opponent has an equal chance of moving any pieces to any adjacent spot
-    for i in range(d):
-        for j in range(len(prob)):
-            new_prob[i][j] = (1 - 1 / c) * prob[i][j]
-            sum = 0
-            for x in range(max(0, i - 1), min(d, i + 2)):
-                for y in range(max(0, j - 1), min(d, j + 2)):
-                    # Adds to sum
-                    sum += prob[x][y] * 1 / (c * neighbors[x][y])
-            new_prob[i][j] += sum
-
-    # Normalize the probability to ensure Bayes rule is satisfied
-    prob = normalize_prob(new_prob, c)
-    return prob
-
-
-# Function that updates the probability tables after the player has moved
-# Occupied = a list of all the cells currently occupied by the player's units
-# prob_table = dictionary of all the probability tables
-# Size = length of the board
-def update_after_player(occupied, prob_table, size):
-    # units is a string of all of the observations
-    # max_units is a dictionary of the max number of pieces that can be adjacent to one of our units
-    # adjacent is a list that will contain all the adjacent cells to an occupied cell that yields an observation
-    units = ""
-    max_units = {"O": 0, "W": 0, "H": 0, "M": 0}
-    adjacent = []
-    # First finds all of the occupied cells that yield observations
-    for (x, y) in occupied:
-        # Observe should return one of the following: "b", "s", "n", "h", or ""
-        observation = board.Board.observe(x, y)
-
-        # If there is a relevant observation, add all of its adjacent cells to the adjacent list
-        if observation != "":
-            for i in range(max(0, x - 1), min(size, x + 2)):
-                for j in range(max(0, y - 1), min(size, y + 2)):
-                    adjacent.append(i, j)
-
-            # Increment the max number of units
-            max_units[observation] += 1
-            # Otherwise set all of the probabilities of the adjacent cells to 0
-        else:
-            for i in range(max(0, x - 1), min(size, x + 2)):
-                for j in range(max(0, y - 1), min(size, y + 2)):
-                    prob_table['W'][i][j] = 0
-                    prob_table['M'][i][j] = 0
-                    prob_table['H'][i][j] = 0
-                    prob_table['O'][i][j] = 0
-
-    # These functions find all the possible combinations of cells and units that could result from our observations
-    data = [0] * len(units)
-    combos = []
-    combo_locations(adjacent, combos, data, 0, len(adjacent) - 1, 0, len(units))
-    unit_combos = get_unit_combos(units)
-
-    # Find the alpha values
-    alphas = {"O": get_alpha(prob_table["O"]), "W": get_alpha(prob_table["W"]), "M": get_alpha(prob_table["M"]),
-              "H": get_alpha(prob_table["H"])}
-
-    # Calculates the P(O) value, then resets the probabilities on the board accordingly
-    p_o = get_p_o(prob_table, unit_combos, combos, alphas, max_units)
-    for (x, y) in adjacent:
-        prob_table["W"][x][y] *= p_o
-        prob_table["M"][x][y] *= p_o
-        prob_table["H"][x][y] *= p_o
-        prob_table["O"][x][y] *= p_o
+    return ret
